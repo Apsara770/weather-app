1f9769f900680ea70d1e0844c2ecc68a34f61085
Sandamini Weerasekara <sewwandiapsara171@gmail.com>
2026-04-18 13:37:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getWeather() {
 
     const city = document.getElementById("cityInput").value;
-    const apiKey = "c048a8b270f379cc5e5f3da023d456b2";
+    const apiKey = "YOUR_API_KEY_HERE";
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
