9c499cbab8c75c506349466583be408a0a3fd65f
Sandamini Weerasekara <sewwandiapsara171@gmail.com>
2026-04-19 07:20:26 +0530

    Change API key in getWeather function
    Updated the API key for the weather API.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getWeather() {
 
     const city = document.getElementById("cityInput").value;
-    const apiKey = "YOUR_API_KEY_HERE";
+    const apiKey = "c048a8b270f379cc5e5f3da023d456b2";
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
